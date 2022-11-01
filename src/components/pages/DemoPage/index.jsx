f97ffbdb703904b269a0bf7a26b701d4b29855ca
NicolasOmar <nicolas.passerino@gmail.com>
2022-11-01 19:00:16 -0300

    Agregando logica para llamar API desde la página

--- src/components/pages/DemoPage/index.jsx
+++ src/components/pages/DemoPage/index.jsx
@@ -1,10 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 // COMPONENTES
 import Site from '../../templates/Site'
 // CONFIGURACIÓN
 import siteConfig from './configData.json'
 
 const DemoPage = () => {
-  return <Site useDummyData={false} siteConfig={siteConfig} />
+  const [queryWord, setQueryWord] = useState(null)
+  const [loadedCards, setloadedCards] = useState([])
+
+  useEffect(() => {
+    const callApi = async () => {
+      const response = await axios.get(`https://api.unsplash.com/search/photos`, {
+        headers: { Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}` },
+        params: { query: queryWord }
+      })
+
+      setloadedCards(
+        response.data.results.map(({ description, links }) => ({
+          content: <p>{description}</p>,
+          image: {
+            src: links.html
+          }
+        }))
+      )
+    }
+
+    queryWord && callApi()
+  }, [queryWord])
+
+  return (
+    <Site
+      useDummyData={false}
+      siteConfig={siteConfig}
+      cards={loadedCards}
+      onClick={word => setQueryWord(word)}
+    />
+  )
 }
 
 export default DemoPage
